4f3c79cb9762529b55d6f99a523ee805bb7646de
Mikayel <m.13021999@mail.ru>
2025-01-12 16:49:58 +0300

    постипенно делаю шляпу

--- src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
+++ src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
@@ -2,9 +2,8 @@ package ru.kata.spring.boot_security.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories
+
 @SpringBootApplication
 public class SpringBootSecurityDemoApplication {
 
